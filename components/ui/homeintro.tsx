25e990114d6d99b2c249e5d691d8bd2a74881d41
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-09-13 19:37:16 +0530

    eslit

--- components/ui/homeintro.tsx
+++ components/ui/homeintro.tsx
@@ -7,12 +7,13 @@ export const HomeIntro = () => {
       <section className="md:flex md:items-center md:space-x-8 ">
         <div className="p-10">
           <h1 className="font-meduim text-3xl md:text-5xl mb-4 text-black">
-            Hi, I'm
+            Hi, I&apos;m
             <span className="text-blue-500"> Venkatesh</span>
           </h1>
+
           <div className="col-span-3 max-w-2xl">
             <p className="px-0  text-lg text-left text-gray-600 md:leading-8 mb-4">
-              I'm a software engineer from India, specializing in JavaScript,
+              I&apos;m a software engineer from India, specializing in JavaScript,
               TypeScript, React, and 69 other technologies. Passionate about
               serverless architectures and full-stack development, I’m always
               exploring new technologies. I do fullstack and a bit of everything
@@ -20,7 +21,7 @@ export const HomeIntro = () => {
             </p>
 
             <div className="mb-4">
-              <SocialLink/>
+              <SocialLink />
             </div>
             <div className="space-x-0  sm:space-x-2 sm:space-y-0 ">
               <button className="border mr-4 focus:ring-4 text-blue-600 hover:bg-blue-500 hover:text-white font-bold border-blue-400 p-4 rounded-md  text-bold">
