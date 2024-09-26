9a0e5cfe962b71dfa26857e1bd964ecfe0a5c946
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-09-26 22:14:48 +0530

    port

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -11,9 +11,9 @@ export const HeroBtn = () => {
   return (
     <button
       onClick={handleClick}
-      className="inline-flex py-2 px-5 sm:py-4 md:py-6 sm:px-7 md:px-9 animate-shimmer items-center text-2xl justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 gap-2"
+      className="inline-flex  py-2 px-5 sm:py-4 md:py-6 sm:px-7 md:px-9 animate-shimmer items-center text-2xl justify-center rounded-md border  border-slate-800 dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 gap-2"
     >
-      <pre>npx venkatesh2100</pre> <ClipboardCopyIcon />
+      <pre className=''>npx venkatesh2100</pre> <ClipboardCopyIcon className='hover:rotate-12' />
     </button>
   );
 };
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -80,35 +80,38 @@ export default function Navbar() {
           >
             Resume
           </a>
+
         </div>
 
         {/* Button for hiring on all screens */}
         <div
-          onClick={() => {
-            router.push(`/api/auth/signin`);
-          }}
-          className="hidden lg:flex dark:bg-white  text-indigo-600 font-bold border rounded-md p-2 md:p-4"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
-            />
-          </svg>
+          onClick={() => router.push(`/api/auth/signin`)}
+          className="hidden lg:flex items-center hover:rotate-12 justify-center space-x-2 dark:bg-white text-black-600 font-bold border rounded-md px-4 py-2 transition-all duration-300 hover:bg-indigo-50 dark:hover:bg-grey-600 hover:text-black shadow-lg dark:hover:shadow-pink-500/50 white:hover:shadow-indigo-500/50"
 
-          <span>Hire me</span>
+        >
+          <div className="relative group">
+            <div className="absolute inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+            <div className="relative z-10 flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 hover:rotate-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                />
+              </svg>
+              <span className="z-10">Hire me</span>
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* Mobile menu */}
       <div
         className={`fixed inset-0 bg-white shadow-md z-50 p-6 transition-transform transform ${
           isMenuOpen ? "translate-y-0" : "-translate-y-full"
@@ -139,6 +142,32 @@ export default function Navbar() {
           >
             Resume
           </a>
+          <div
+          onClick={() => router.push(`/api/auth/signin`)}
+          className="lg:flex items-center hover:rotate-12 justify-center space-x-2 dark:bg-white text-black-600 font-bold border rounded-md px-4 py-2 transition-all duration-300 hover:bg-indigo-50 dark:hover:bg-grey-600 hover:text-black shadow-lg dark:hover:shadow-pink-500/50 white:hover:shadow-indigo-500/50"
+
+        >
+          <div className="relative group">
+            <div className="absolute inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+            <div className="relative z-10 flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 hover:rotate-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                />
+              </svg>
+              <span className="z-10">Hire me</span>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </nav>
